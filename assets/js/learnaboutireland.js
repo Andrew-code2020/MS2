47b26c90544743cbf7eefc010a9a6d4823c9c99e
Andrew Burns <burnsad@tcd.ie>
2020-08-01 16:05:55 +0000

    toggle function added to history section of page. CI advised re div if picture background is required

--- assets/js/learnaboutireland.js
+++ assets/js/learnaboutireland.js
@@ -1,11 +1,29 @@
 $(document).ready(function () {
-  $("#hide").click(function () {
-    $("p").hide(slow);
-  });
-  $("#show").click(function () {
-    $("p").show(slow);
-  });
+    $(".19thcentury").hide();
+    $(".20thcentury").hide();
+    $(".21stcentury").hide();
 });
+
+$(".toggleButton19").click(function (){
+    $(".19thcentury").next().toggle("slow");
+});
+
+$(".toggleButton20").click(function (){
+    $(".20thcentury").toggle("slow");
+}); 
+
+
+$(".toggleButton21").click(function (){
+    $(".21stcentury").toggle("slow");
+});
+
+/***$(".toggleButton").click(function (){
+
+    $(this).next("p[class$='century']").toggle("slow");
+    $(this).next("p[class$='century']").toggle("slow");
+    $(this).next("p[class$='century']").toggle("slow");
+
+});***/
 
 /*--$( ".history" ).click(function() {
   $( this ).replaceWith( $( `<p class="factone"> Irish Republican Army (IRA), launched a three-year guerrilla war, which ended in
