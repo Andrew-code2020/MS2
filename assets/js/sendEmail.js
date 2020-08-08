32b19e471b89a34a77b69fce38ea7b644e848bd3
Andrew Burns <burnsad@tcd.ie>
2020-08-08 16:13:46 +0000

    Email JS sorted and powerscourt added to the Google Maps. reset script added sendMail function

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,15 +1,15 @@
-function sendMail (contactForm){
+function sendMail(contactForm){
     emailjs.send("gmail", "Holiday in Ireland", {
         "from_name":contactForm.from_name.value,
         "from_email": contactForm.from_email.value, 
         "holiday_ire": contactForm.holiday_ire.value,
         "message_ire": contactForm.message_ire.value,
         "gridRadios1": contactForm.gridRadios1.value,
-        "gridRadios2": contactForm.gridRadios2.value
     })
     .then(
         function(response){
             console.log("SUCCESS", response);
+            $('#contactForm').trigger("reset");
         },
         function(error){
             console.log("FAILED", error);
