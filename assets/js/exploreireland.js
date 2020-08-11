74a3a48cca82ed1605bfd0de22919ac1ea51b6ce
Andrew Burns <burnsad@tcd.ie>
2020-08-11 18:45:40 +0000

    FAQ.html added and initial content added. Revisions to follow and JS Slide up and down.FAQ navigation link added. CSS style for exploreireland refined

--- assets/js/exploreireland.js
+++ assets/js/exploreireland.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     $(".activity2").hide();
-    
+    $(".culture2").hide();
+    $(".wellness2").hide();
+    $(".cities2").hide();    
 });
 
 $(".activity" )
@@ -11,7 +13,29 @@ $(".activity" )
     $(".activity2").hide();
   });
 
+$(".culture" )
+  .mouseenter(function() {
+    $(".culture2").show();
+  })
+  .mouseleave(function() {
+    $(".culture2").hide();
+  });
 
+  $(".wellness" )
+  .mouseenter(function() {
+    $(".wellness2").show();
+  })
+  .mouseleave(function() {
+    $(".wellness2").hide();
+  });
+
+  $(".cities")
+  .mouseenter(function() {
+    $(".cities2").show();
+  })
+  .mouseleave(function() {
+    $(".cities2").hide();
+  });
 
 
 /*function initMap() {
