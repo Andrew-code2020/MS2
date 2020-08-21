440d1a78e90e8572012f7f3a7954960ee0cf067a
Andrew Burns <burnsad@tcd.ie>
2020-08-21 10:10:19 +0000

    FAQ buttons added and Jquery toggle to reveal answers to. css style sheet reviewed and organised

--- assets/js/FAQ.js
+++ assets/js/FAQ.js
@@ -1,15 +1,44 @@
 $(document).ready(function () {
     $(".airports").hide();
     $(".transport").hide();
-    $(".activityinfo").hide();
+    $(".currency").hide();
+    $(".activityhol").hide();
+    $(".culturehol").hide();
+    $(".wellnesshol").hide();
+    $(".cityhol").hide();
 });
 
-$(".card-body").click(function() {
+$(".airports1").click(function() {
+  $( ".airports" ).toggle( "slow", function() {
+  });
+});
+
+$(".transport1").click(function() {
   $( ".transport" ).toggle( "slow", function() {
   });
 });
 
-$(".card-body").click(function() {
-  $( ".activityinfo" ).toggle( "slow", function() {
+$(".currency1").click(function() {
+  $( ".currency" ).toggle( "slow", function() {
+  });
+});
+
+$(".activityhol1").click(function() {
+  $( ".activityhol" ).toggle( "slow", function() {
+  });
+});
+
+$(".culturehol1").click(function() {
+  $( ".culturehol").toggle( "slow", function() {
+  });
+});
+
+$(".wellnesshol1").click(function() {
+  $(".wellnesshol").toggle( "slow", function() {
+  });
+});
+
+$(".cityhol1").click(function() {
+  $( ".cityhol").toggle( "slow", function() {
   });
 });
